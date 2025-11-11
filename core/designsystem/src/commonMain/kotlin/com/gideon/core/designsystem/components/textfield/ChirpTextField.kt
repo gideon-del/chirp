b14d637a4f84631030bb34801e9c72d676583d88
gideon-del <gideonchidi471@gmail.com>
2025-11-11 18:35:22 +0100

    feat: chirp password text field

--- core/designsystem/src/commonMain/kotlin/com/gideon/core/designsystem/components/textfield/ChirpTextField.kt
+++ core/designsystem/src/commonMain/kotlin/com/gideon/core/designsystem/components/textfield/ChirpTextField.kt
@@ -45,27 +45,16 @@ fun ChirpTextField(
     keyboardType: KeyboardType = KeyboardType.Text,
     onFocusChanged: (Boolean) -> Unit = {},
 ) {
-    val interactionSource = remember {
-        MutableInteractionSource()
-    }
-    
-    val isFocused by interactionSource.collectIsFocusedAsState()
 
+    ChirpTextFieldLayout(
+        title = title,
+       isError = isError,
+       onFocusChanged = onFocusChanged,
+       supportingText = supportingText,
+        modifier = modifier,
+       enabled = enabled
 
-    LaunchedEffect(isFocused) {
-        onFocusChanged(isFocused)
-    }
-    Column(
-        modifier =modifier
-    ) {
-       if(title != null) {
-           Text(
-               text = title,
-               style = MaterialTheme.typography.labelSmall,
-               color = MaterialTheme.colorScheme.extended.textSecondary
-           )
-           Spacer(modifier = Modifier.height(8.dp))
-       }
+    ) { styledModifier,interactionSource ->
 
         BasicTextField(
             state = state,
@@ -85,29 +74,8 @@ fun ChirpTextField(
                 keyboardType = keyboardType
             ),
             cursorBrush = SolidColor(MaterialTheme.colorScheme.onSurface),
-           interactionSource = interactionSource,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color =  when {
-                        isFocused -> MaterialTheme.colorScheme.primary.copy(
-                            alpha = 0.05f
-                        )
-                        enabled -> MaterialTheme.colorScheme.surface
-                        else -> MaterialTheme.colorScheme.extended.secondaryFill
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .border(
-                    width = 1.dp,
-                    color = when {
-                        isError -> MaterialTheme.colorScheme.error
-                        isFocused -> MaterialTheme.colorScheme.primary
-                        else -> MaterialTheme.colorScheme.outline
-                    },
-                    shape = RoundedCornerShape(8.dp)
-                )
-                .padding(12.dp),
+            interactionSource = interactionSource,
+            modifier = styledModifier ,
             decorator = { innerBox ->
                 Box(
                     modifier = Modifier
@@ -127,16 +95,8 @@ fun ChirpTextField(
             }
 
         )
-        if(supportingText != null) {
-            Spacer(modifier = Modifier.height(4.dp))
-            Text(
-                text = supportingText,
-                color = if(isError) MaterialTheme.colorScheme.error
-                else MaterialTheme.colorScheme.extended.textTertiary,
-                style = MaterialTheme.typography.bodySmall
-            )
-        }
     }
+
 }
 
 @Composable
