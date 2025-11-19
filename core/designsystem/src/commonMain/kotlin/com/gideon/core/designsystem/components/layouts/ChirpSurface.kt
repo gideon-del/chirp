8382a446a8ef287e82155f173be1cde5b28afe77
gideon-del <gideonchidi471@gmail.com>
2025-11-19 13:25:28 +0100

    feat: chirp surface component

---
+++ core/designsystem/src/commonMain/kotlin/com/gideon/core/designsystem/components/layouts/ChirpSurface.kt
@@ -0,0 +1,94 @@
+package com.gideon.core.designsystem.components.layouts
+
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import chirp_mobile_app.core.designsystem.generated.resources.Res
+import chirp_mobile_app.core.designsystem.generated.resources.logo_chirp
+import com.gideon.core.designsystem.theme.ChirpTheme
+import org.jetbrains.compose.resources.vectorResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ChirpSurface(
+    modifier: Modifier = Modifier,
+    header: @Composable ColumnScope.() -> Unit = {},
+    content: @Composable  ColumnScope.() -> Unit,
+) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+
+        ) {
+            header()
+
+            Surface(
+                color = MaterialTheme.colorScheme.surface,
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .clip(
+                        RoundedCornerShape(
+                            topStart = 20.dp,
+                            topEnd = 20.dp
+                        )
+                    )
+            ) {
+
+                Column(
+                    modifier= Modifier.fillMaxSize()
+                ) {
+
+                    content()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ChirpSurfacePreview() {
+    ChirpTheme {
+        ChirpSurface(
+            modifier = Modifier
+                .fillMaxSize(),
+            header = {
+                Icon(
+                    imageVector = vectorResource(Res.drawable.logo_chirp),
+                    contentDescription = null,
+                    tint = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier
+                        .padding(vertical = 32.dp)
+                )
+            }
+        ) {
+            Text(
+                text="Welcome to Chirp!",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .padding(vertical = 40.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+        }
+    }
+}
